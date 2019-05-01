ca82511c1213d05f984c151507e304bd5bd235b6
ZeroNoMe <jgonzalez@f2tc.com>
2019-05-01 11:36:32 -0400

    Stable proto-skeleton.

--- poly_map/mapper.py
+++ poly_map/mapper.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# img=mpimg.imread('../gtasa-blank-vector.png')
+# imgplot = plt.imshow(img)
+# plt.show()
 import numpy as np
 import cv2
 image = cv2.imread("../gtasa-blank-vector.png")
-blue = (255, 0, 0)
-los_santos = np.array([[1123.07,948.874],[794.348,1018.21],[800.009,1535.48],[1535.5,1535.48],[1535.72,1036.76],[1123.07,948.87]], np.int32)
-cv2.polylines(image, [los_santos], True, blue, thickness=30)
-# cv2.imwrite('test.png', image)
-cv2.namedWindow('blue polygon', cv2.WINDOW_NORMAL)
-cv2.imshow("blue polygon", image)
+grove = (50, 200, 50); ballas = (140, 0, 140); vagos = (0, 215, 255); aztecas = (225, 255, 0); mcb = (70, 70, 70); rgt = (60, 20, 220); dnb = (11, 134, 184); rifas = (170, 178, 32); leone = (0, 0, 130); forelli = (150, 0, 0); sindacco = (13, 13, 13)
+ganton = np.array([[1438.11,1186.01],[1438.11,1239.6],[1337.11,1239.6],[1337.11,1186.01],[1438.11,1186.01]], np.int32)
+cv2.fillPoly(image, [ganton], sindacco)
+cv2.namedWindow('test map', cv2.WINDOW_NORMAL)
+cv2.imshow("test map", image)
 cv2.waitKey(0)
+cv2.imwrite('test_map.png', image)
